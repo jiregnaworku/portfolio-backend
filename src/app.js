c6e1de5f6d4ce474f2faa1d314aec9f3009e7e3e
jiregna worku <jiregna123w@gmail.com>
2025-10-19 20:58:22 +0300

    CORS Error fixed

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   optionsSuccessStatus: 200
 }));
